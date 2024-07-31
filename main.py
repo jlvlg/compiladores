5b97f2fd3d4c707ee7851220a3a450dda5796711
Lucas Vinícius <j.lucasvinicius03@gmail.com>
2024-07-31 09:31:27 -0300

    Added meaningful error messages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from lexer import Lexer
 from parser import Parser
+from pprint import pprint
 
 with open('test') as f:
     tokens = Lexer().scan(f.read())
-    Parser().parse(tokens, True)
+    pprint(Parser().parse(tokens, True))
